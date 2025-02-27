02db37cba8a7c6e5b95a50f484aa466b79256df6
Jesper <jesper.l.richert@gmail.com>
2025-02-27 17:07:50 +0100

    Update to 1.21.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,18 +3,20 @@ import net.minecrell.pluginyml.paper.PaperPluginDescription
 plugins {
     kotlin("jvm") version "2.1.0"
     id("com.gradleup.shadow") version "9.0.0-beta2"
-    id("io.papermc.paperweight.userdev") version "1.7.5"
     id("xyz.jpenilla.run-paper") version "2.3.1"
     id("net.minecrell.plugin-yml.paper") version "0.6.0"
     kotlin("plugin.serialization") version "2.1.0"
 }
 
 group = "net.crystopia"
-version = "1.0.0"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/")
+    maven {
+        name = "papermc"
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
     maven("https://oss.sonatype.org/content/groups/public/")
     maven("https://repo.flyte.gg/releases")
     maven("https://repo.codemc.io/repository/maven-snapshots/")
@@ -34,7 +36,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    paperweight.paperDevBundle("1.21.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
 
     implementation("net.kyori:adventure-api:4.17.0")
 
@@ -55,7 +57,6 @@ kotlin {
 
 tasks.build {
     dependsOn(tasks.shadowJar)
-    dependsOn(tasks.reobfJar)
 }
 
 tasks.shadowJar {
@@ -79,12 +80,9 @@ tasks.shadowJar {
     }
 }
 
-paperweight.reobfArtifactConfiguration = io.papermc.paperweight.userdev.ReobfArtifactConfiguration.REOBF_PRODUCTION
-
-
 paper {
     name = "AffiliateCode"
-    version = "1.0"
+    version = "1.1.0"
     description = "Affiliate Plugin to reward player they hire player."
     main = "net.crystopia.affiliatecode.AffiliateCode"
     foliaSupported = false
